c2a01fe93a31ce89906f8e8f43ad8a2a48335c98
Mar Gal <mgalloperalta@gmail.com>
2018-10-04 20:26:39 +0200

    loop exercise, toUpper, split, reverse

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -7,42 +7,56 @@
 
 // Print "The navigator's name is YYYY"
 
-var hacker1 = 'Pedro';
-
-console.log("The driver's name is XXXX");
-
-var hacker2 = prompt("What's the navigators name:");
-
-console.log("The navigator's name is YYYY");
-
-
-if (hacker1.length>hacker2.length){
-  console.log("The Driver has the longest name, it has " + hacker1.length + "characters");
-} else if (hacker2.length>hacker1.length){
-  console.log("Yo, navigator got the longest name, it has " + hacker2.length + "characters");
-}else{
-  console.log("wow, you both got equally long names, " + hacker1.length + "characters!!");
-}
-
-
 // Conditionals
 // Depending on which name is longer, print:
 // The Driver has the longest name, it has XX characters or
 // Yo, navigator got the longest name, it has XX characters or
 // wow, you both got equally long names, XX characters!!
 
+var hacker1 = 'Pedro';
+
+// console.log("The driver's name is XXXX");
+
+// // var hacker2 = prompt("What's the navigators name:");
+
+// console.log("The navigator's name is YYYY");
 
 
-// Loops
+// if (hacker1.length>hacker2.length){
+//   console.log("The Driver has the longest name, it has " + hacker1.length + "characters");
+// } else if (hacker2.length>hacker1.length){
+//   console.log("Yo, navigator got the longest name, it has " + hacker2.length + "characters");
+// }else{
+//   console.log("wow, you both got equally long names, " + hacker1.length + "characters!!");
+// }
+
+
+// ---------------------- LOOPS ---------------------------------
 // Print all the characters of the driver's name, separated by a space and in capitals ie. "J O H N"
 
+
+console.log(hacker1.toUpperCase(' ').split(''));
+
 // Print all the characters of the navigator's name, in reverse order. ie. "nhoJ"
+
+var newHacker = '';
+
+for (var i = hacker1.length - 1; i >= 0; i--){
+   newHacker += hacker1[i];
+}
+
+console.log(newHacker);
 
 // Depending on the lexicographic order of the strings, print:
 
 // The driver's name goes first
 // Yo, the navigator goes first definitely
 // What?! You both got the same name?
+
+
+
+
+
 // Bonus Time!
 // Ask the user for a new string and check if it's a Palindrome. Examples of palindromes:
 
